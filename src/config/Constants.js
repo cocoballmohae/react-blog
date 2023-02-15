60d6113d6e231a0e4269d41b21817178c28111b9
km <8721son@naver.com>
2023-02-15 12:36:31 +0900

    Join 완성, login page

---
+++ src/config/Constants.js
@@ -0,0 +1,1 @@
+export const BASE_URL = "http://gangmii.com:8080";
--- src/pages/Join.js
+++ src/pages/Join.js
@@ -1,7 +1,9 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Button, Card, Col, Form, InputGroup, Row } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 import LogoImg from "../assets/logo.png";
 import CommonLayout from "../layouts/CommonLayout";
+import { customAxios } from "../utils/CustomAxios";
 
 // 회원가입
 const Join = () => {
@@ -13,11 +15,95 @@ const Join = () => {
     simpleDescElement: null,
   });
 
+  const navigate = useNavigate();
+
+  const validateFields = () => {
+    // validation check
+    // 1. 빈 값이 없는지
+    // 2. id,pw 형식에 맞는지 (영어,숫자 조합 몇글자 이상) - 정규식
+    // 3. pw 두번 입력하는거 두개 같은지
+    const { idElement, pwElement, pw2Element, simpleDescElement } =
+      refs.current;
+
+    if (idElement.value === "") {
+      alert("아이디를 입력해주세요.");
+      idElement.focus();
+      return false;
+    }
+
+    if (pwElement.value === "") {
+      alert("비밀번호를 입력해주세요.");
+      pwElement.focus();
+      return false;
+    }
+
+    if (pw2Element.value === "") {
+      alert("비밀번호 확인을 입력해주세요.");
+      pw2Element.focus();
+      return false;
+    }
+
+    if (simpleDescElement.value === "") {
+      alert("한 줄 소개를 입력해주세요.");
+      simpleDescElement.focus();
+      return false;
+    }
+
+    if (pwElement.value !== pw2Element.value) {
+      alert("비밀번호가 일치하지 않습니다.");
+      pw2Element.focus();
+      return false;
+    }
+
+    return true;
+  };
+
   const requestJoin = () => {
     const { idElement, pwElement, pw2Element, simpleDescElement } =
       refs.current;
-    console.log(idElement.value);
-    console.log("회원가입 요청");
+    // validateFields === false
+    if (!validateFields()) {
+      return;
+    }
+
+    // 서버 통신
+    // 1. jquery ajax
+    // 2. js fetch
+    // 3. 외부 라이브러리 axios
+
+    const user = {
+      id: idElement.value,
+      password: pwElement.value,
+      simpleDescElement: simpleDescElement.value,
+    };
+    // publicAxios : 서버랑 통신
+    // then : 예상 가능한 에러 처리
+    // catch : 예상 못한 에러 처리
+    // finally : 에러가 뜨든 안 뜨든 실행할 코드
+    customAxios
+      .publicAxios({
+        method: "post",
+        url: "/v1/api/user/join",
+        data: user,
+      })
+      .then((response) => {
+        console.log(response);
+        if (response.status === 200) {
+          alert("회원가입에 성공했습니다.");
+          navigate("/login");
+        } else {
+          alert(response.data.message);
+        }
+      })
+      .catch((error) => {
+        // ?(옵셔널 체이닝) => null이 아닐경우에만 반환 / null -> undefined
+        if (error?.response?.data?.detail != null) {
+          alert(JSON.stringify(error?.response?.data?.detail));
+        } else {
+          alert("알 수 없는 오류");
+        }
+      })
+      .finally(() => {});
   };
   //순수 리액트 훅스 x
   // const qs = useLocation();
